ed7844184ac5c734b64f421eaec5601cbd3743d7
www-data <www-data@localhost.localdomain>
2018-04-23 23:05:11 +0800

    Update From PublicAccount

--- isee.py
+++ isee.py
@@ -132,7 +132,7 @@ photos: images/girl.jpg\n---\n\n";
 
     sourceFile.close();
     destFile.close();
-    #os.chdir(sys.path[0]);
+    os.chdir(sys.path[0]);
     os.system("/usr/bin/git pull origin HexoBackup");
     os.system("/usr/bin/git add .");
     os.system("/usr/bin/git commit -m 'Update From PublicAccount'");
